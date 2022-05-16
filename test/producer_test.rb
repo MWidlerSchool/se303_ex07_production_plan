a9f43616ac2c7fea7c752399e935fad2f53bd5a7
WireHallMedic <michaelwidler@yahoo.com>
2022-05-15 19:02:11 -0700

    add test_zero_demand

--- test/producer_test.rb
+++ test/producer_test.rb
@@ -2,6 +2,8 @@ gem 'minitest', '~> 5.4'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/producer'
+require_relative '../lib/province'
+require_relative '../lib/data'
 
 class ProducerTest < Minitest::Test
 
@@ -9,4 +11,9 @@ class ProducerTest < Minitest::Test
       @asia = Province.new(sample_province_data)
     end
 
+    def test_zero_demand
+        @asia.demand = 0
+        assert_equal(-25, @asia.shortfall)
+        assert_equal(0, @asia.profit)
+    end
 end
